df6bf63147afbcc15b3704b4ee82aafc2dbcdf31
houssemJammeli <houssemjammeli037@gmail.com>
2026-03-09 13:56:21 +0100

    tests5

--- GestionStock/GestionStock/obj/Debug/net8.0/GestionStock.AssemblyInfo.cs
+++ GestionStock/GestionStock/obj/Debug/net8.0/GestionStock.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionStock")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5a642fb08e368f8109f628ddef8e16a3fe72189")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b63fbdffd0fbab1874b2f5d072051f67f6854e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionStock")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionStock")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
